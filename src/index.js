0bf870b2e5238a51e5c9e02b59051908bf545436
Agatha Riesenbürger <riesenburgeragatha34@gmail.com>
2024-03-11 20:23:32 +0100

    finised with the Basic layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <Router>
